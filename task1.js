b38d27262dbecc426356be75b9eea09431178a80
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-07-26 17:49:29 +0530

    task3

--- task1.js
+++ task1.js
@@ -5,6 +5,19 @@ var arr = [{Company:"Samsung",Model:"Galaxy",Memory:64,Price:15000},
 {Company:"Apple",Model:"S12",Memory:64,Price:25000}];
 design();
 
+function populate(){
+    var text="";
+text += "<table><tr><th>Company</th><th>Model</th><th>Memory (GB)</th><th>Price (Rs)</th></tr>";
+for(i=0;i<arr.length;i++){
+    text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
+    +arr[i].Price+"</td></tr>";
+}
+text += "</table>";
+
+document.getElementById('container').innerHTML=text;
+
+}
+
 function design(args){
 var text="";
 text += "<table><tr><th>Company</th><th>Model</th><th>Memory (GB)</th><th>Price (Rs)</th></tr>";
@@ -17,6 +30,8 @@ text += "</table>";
 document.getElementById('container').innerHTML=text;
 }
 
+// task2  
+
 function selectproducts(){
 var selectproduct = document.getElementById('selectproducts');
 var index = selectproduct.selectedIndex;
@@ -27,7 +42,6 @@ for(i=0;i<arr.length;i++){
         if(document.getElementById('productsquantity').value == arr[i].Company){
             text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
     +arr[i].Price+"</td></tr>";
-    console.log(text);
     document.getElementById('searchtable').innerHTML+=text;
         }
     }
@@ -54,6 +68,81 @@ for(i=0;i<arr.length;i++){
     +arr[i].Price+"</td></tr>";
     document.getElementById('searchtable').innerHTML+=text;
     }
+    
 }
+
+}
+document.getElementById('selectproducts').value="-Select field-";
+    document.getElementById('productsquantity').value="";
+
+}
+// task3
+
+function sorted(){
+var selectsort1 = document.getElementById('sort1').selectedIndex;
+var selectsort2 = document.getElementById('sort2').selectedIndex;
+if(selectsort1==1){
+    if(selectsort2==1){
+        arr.sort((a,b)=>{
+            let fa = a.Company.toLowerCase(),
+            fb = b.Company.toLowerCase();
+            if(fa<fb){return -1;}
+            if(fa>fb){return 1;}
+            return 0;});
+            populate();
+    }
+    if(selectsort2==2){
+        arr.sort((a,b)=>{
+            let fa =a.Model.toLowerCase(),
+            fb = b.Model.toLowerCase();
+            if(fa<fb){return -1;}
+            if(fa>fb){return 1;}
+            return 0;});
+            populate();
+    }
+    if(selectsort2==3){
+        arr.sort((a,b)=>{
+            return a.Memory-b.Memory;
+        });
+        populate();
+    }
+    if(selectsort2==4){
+        arr.sort((a,b)=>{
+            return a.Price-b.Price;
+        });
+        populate();
+    }
+}
+if(selectsort1==2){
+    if(selectsort2==1){
+        arr.sort((a,b)=>{
+            let fa = a.Company.toLowerCase(),
+            fb = b.Company.toLowerCase();
+            if(fb<fa){return -1;}
+            if(fb>fa){return 1;}
+            return 0;});
+            populate();
+    }
+    if(selectsort2==2){
+        arr.sort((a,b)=>{
+            let fa =a.Model.toLowerCase(),
+            fb = b.Model.toLowerCase();
+            if(fb<fa){return -1;}
+            if(fb>fa){return 1;}
+            return 0;});
+            populate();
+    }
+    if(selectsort2==3){
+        arr.sort((a,b)=>{
+            return b.Memory-a.Memory;
+        });
+        populate();
+    }
+    if(selectsort2==4){
+        arr.sort((a,b)=>{
+            return b.Price-a.Price;
+        });
+        populate();
+    }
 }
 }
