6855b8688552344bb9168ab6b27be12390fafeef
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-07-26 14:24:24 +0530

    task2

--- task1.js
+++ task1.js
@@ -1,4 +1,3 @@
-
 var arr = [{Company:"Samsung",Model:"Galaxy",Memory:64,Price:15000},
 {Company:"Nokia",Model:"S730",Memory:128,Price:22000},
 {Company:"Xiomi",Model:"Note",Memory:32,Price:12000},
@@ -16,4 +15,45 @@ for(i=0;i<arr.length;i++){
 text += "</table>";
 
 document.getElementById('container').innerHTML=text;
+}
+
+function selectproducts(){
+var selectproduct = document.getElementById('selectproducts');
+var index = selectproduct.selectedIndex;
+
+for(i=0;i<arr.length;i++){
+    if(index == 1){
+        var text ="";
+        if(document.getElementById('productsquantity').value == arr[i].Company){
+            text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
+    +arr[i].Price+"</td></tr>";
+    console.log(text);
+    document.getElementById('searchtable').innerHTML+=text;
+        }
+    }
+    if(index==2){
+        var text="";
+        if(document.getElementById('productsquantity').value == arr[i].Model){
+            text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
+    +arr[i].Price+"</td></tr>";
+    document.getElementById('searchtable').innerHTML+=text;
+        }
+    }
+    if(index==3){
+        var text="";
+        if(document.getElementById('productsquantity').value == arr[i].Memory){
+            text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
+    +arr[i].Price+"</td></tr>";
+    document.getElementById('searchtable').innerHTML+=text;
+        }
+    }
+    if(index==4){
+        var text="";
+        if(document.getElementById('productsquantity').value == arr[i].Price){
+            text += "<tr><td>"+arr[i].Company+"</td><td>"+arr[i].Model+"</td><td>"+arr[i].Memory+"</td><td>"
+    +arr[i].Price+"</td></tr>";
+    document.getElementById('searchtable').innerHTML+=text;
+    }
+}
+}
 }
