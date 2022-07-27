9f2d6c8028bcd0fd6c8ee6581cbaeb7d3bb72920
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-07-27 10:42:34 +0530

    modifiedtask4

--- task1.js
+++ task1.js
@@ -150,11 +150,17 @@ if(selectsort1==2){
 // task4
 function newdetails(){
     var company = document.getElementById('inputcompany').value;
-var model = document.getElementById('inputmodel').value;
-var memory = document.getElementById('inputmemory').value;
-var price = document.getElementById('inputprice').value;
+    var model = document.getElementById('inputmodel').value;
+    var memory = document.getElementById('inputmemory').value;
+    var price = document.getElementById('inputprice').value;
 
     const arra = {Company:company,Model:model,Memory:memory,Price:price};
-    arr.push(arra);
+    arr.splice(2,0,arra);
     populate();
+    document.getElementById('inputcompany').value="";
+    document.getElementById('inputmodel').value="";
+    document.getElementById('inputmemory').value="";
+    document.getElementById('inputprice').value="";
 }
+
+
